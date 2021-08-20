c1827eb506eca5e0dff0e43c1a18e16c287d5497
邓江洲 <dengjiangzhouReal@gmail.com>
2021-08-20 17:10:33 +0800

    struct Node{   _   _

---
+++ face/ccc/ccc/main.c
@@ -0,0 +1,70 @@
+//
+//  main.c
+//  ccc
+//
+//  Created by Jz D on 2021/8/20.
+//
+
+#include <stdio.h>
+
+
+
+struct Node{
+    int val;
+    struct Node *left;
+    
+    //  struct Node rhs;    //  Field has incomplete type 'struct Node'
+    
+    //  必须使用， Node *
+    
+    //  否则编译的时候，展开，又是他自己，这个结构体
+    
+    //  无限循环下去
+}root;
+// root 是实例变量
+
+
+
+
+
+struct Tree{
+    int val;
+    struct Node left;
+    
+    //  Must use 'struct' tag to refer to type 'Node'
+    // Node rhs;
+}lemon;
+
+
+
+
+
+
+
+
+typedef struct Wood{
+    int val;
+    struct Node left;
+    
+    //  Must use 'struct' tag to refer to type 'Node'
+    struct Node * rhs;
+}Forest;
+
+//  Forest,  是别名
+
+
+
+
+
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    
+    
+    
+    printf("%d  \n\n", root.val);
+    
+    
+    
+    return 0;
+}
