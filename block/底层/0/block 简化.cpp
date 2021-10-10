408921bd55a2ff1866ec90d8fbd998ca8515c912
邓江洲 <dengjiangzhouReal@gmail.com>
2021-10-10 15:54:21 +0800

    impl.FuncPtr = fp;    //  函数，作为参数，传递

--- block/底层/0/block 简化.cpp
+++ block/底层/0/block 简化.cpp
@@ -1,16 +1,23 @@
 
 
 
+
+
+// block 是对象， 结构体
+
 struct __main_block_impl_0 {
   struct __block_impl impl;
   struct __main_block_desc_0* Desc;
   __main_block_impl_0(void *fp, struct __main_block_desc_0 *desc, int flags=0) {
     impl.isa = &_NSConcreteStackBlock;
     impl.Flags = flags;
-    impl.FuncPtr = fp;
+    impl.FuncPtr = fp;    //  函数，作为参数，传递
     Desc = desc;
   }
 };
+
+
+
 static void __main_block_func_0(struct __main_block_impl_0 *__cself) {
 
         printf(" 6 6  6   \n");
@@ -27,6 +34,12 @@ static struct __main_block_desc_0 {
 
 
 int main(){
+    
+    // __main_block_impl_0
+    
+    // block 是一个构造函数
+
+
     void(*block)(void) = ((void (*)())&__main_block_impl_0((void *)__main_block_func_0, &__main_block_desc_0_DATA));
     ((void (*)(__block_impl *))((__block_impl *)block)->FuncPtr)((__block_impl *)block);
     return 0;
